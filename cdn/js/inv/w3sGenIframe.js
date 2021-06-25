d6a23e89a9b9289b8e61db3d6b7efc5c4ec76e6b
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-25 18:48:01 -0500

    Update w3sGenIframe.js

--- cdn/js/inv/w3sGenIframe.js
+++ cdn/js/inv/w3sGenIframe.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $("<iframe>").attr("src", "https://www.w3schools.com/default.asp").width("100%").height("350px").appendTo("body");
+    $("<iframe>").attr("src", "https://www.w3schools.com/default.asp").width("100%").height("100%").appendTo("body");
 })
