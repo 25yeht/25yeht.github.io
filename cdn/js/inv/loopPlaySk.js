0a451949ceb6530da1a0e3016cba21e416a07b88
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 13:04:13 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -3,18 +3,21 @@ $(document).ready(function() {
 		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
     		$("<button>").text("Get buttons again").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
+			$("#gba").on("click", function() {
+				window.location.reload();
+			});
         	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
 			$("#lp-sk-hi").remove();
         });
     $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
-	$("#gba").on("click", function() {
-		window.location.reload();
-	});
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
             $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
             $("<button>").text("Get buttons again").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
+			$("#gba").on("click", function() {
+				window.location.reload();
+			});
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
         var dasInterval = window.setInterval(function() {
