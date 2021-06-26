7d81cb31bc9f55af99cde6a0bc6a15ce3862efb7
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 11:22:48 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -1,12 +1,14 @@
 if(document.getElementsByName("jquery")[0]) {
     console.log("required.js started!");
-            console.log("Welcome home!");
-            $(document).ready(function() {
-                $("#pl-sk-hi").on("click", function() {
-                    $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
-                    $("#pl-sk-hi").remove();
-                });
-            });
+    console.log("Welcome home!");
+    
+    $(document).ready(function() {
+		$("<button>").attr("id", "pl-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+    	$("#pl-sk-hi").on("click", function() {
+        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
+            $("#pl-sk-hi").remove();
+        });
+    });
 } else {
     console.log("jQuery not found! Maybe try adding the script as the first?");
 }
