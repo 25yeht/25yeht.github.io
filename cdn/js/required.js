4df836cc57c4a0a1c05244c7b3fe92249bfed134
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 11:06:47 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -1,10 +1,9 @@
 if(document.getElementsByName("jquery")[0]) {
     console.log("required.js started!");
             console.log("Welcome home!");
+            $(document).ready(function() {
     //Add favicon
 	$("<link>").attr("rel", "shortcut icon").attr("href", "https://math.afficienta.com/static/favicon.ico").appendTo("head");
-    //Play sky High
-            $(document).ready(function() {
                 $("<button>").text("Play Sky High").attr("id", "pl-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
                 $("#pl-sk-hi").on("click", function() {
                     $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
