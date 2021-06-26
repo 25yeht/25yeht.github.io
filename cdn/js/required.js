f0a28c213f2c5fe480dcf1ef24be16885ac19976
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 12:35:39 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -14,7 +14,11 @@ if(document.getElementsByName("jquery")[0]) {
 		$("h1 .centered").append($("<br>"));
 		$("h1 .centered").append($("<h2").text("This site is currently under development."));
 	    //Loop Sky High Audio
-		$("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+	    var $lpBtn = $("<button>");
+	    $lpBtn.attr("id", "lp-sk-hi");
+	    $lpBtn.text("Loop Sky High");
+	    $("#elektronomia-sky-high-js-player-div").append($lpBtn);
+		//$("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
 		$("#lp-sk-hi").on("click", function() {
 				console.log("Clicked!");
 				$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
