af0b3263a341e4ccc6926b05c569d9a77820bc87
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 12:54:38 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+		//Generate play button and audio
+		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
+    	$("#pl-sk-hi").on("click", function() {
+    		$("<button>").text("Get buttons again").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
+        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
+            $("#pl-sk-hi").remove();
+			$("#lp-sk-hi").remove();
+        });
     $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
