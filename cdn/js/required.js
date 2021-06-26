5ccc0293cc6784c3c12ee743f40d4d6669ee743a
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 12:54:11 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -3,14 +3,7 @@ if(document.getElementsByName("jquery")[0]) {
     console.log("Welcome home!");
     
     $(document).ready(function() {
-		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
-    	$("#pl-sk-hi").on("click", function() {
-    		$("<button>").text("Get buttons again").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
-        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#elektronomia-sky-high-js-player-div");
-            $("#pl-sk-hi").remove();
-			$("#lp-sk-hi").remove();
-        });
+		//Do something
     });
     });
 } else {
