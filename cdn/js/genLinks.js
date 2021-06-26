eea43d4a1bd202721ae04fb5c4c47c711d284322
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 11:33:43 -0500

    Update genLinks.js

--- cdn/js/genLinks.js
+++ cdn/js/genLinks.js
@@ -2,10 +2,10 @@ if(document.getElementsByName("jquery")[0]) {
     console.log("genLinks.js started!");
     $(document).ready(function() {
         console.log("Document is ready!");
-        function addLink(displayName, url) {
+        function addLink(displayName, url, isLink) {
             if(window.location.pathname.charAt(window.location.pathname.length - 1) == "/" && url == window.location.pathname) {
                 var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links");
-            } else if (window.location.pathname.charAt(window.location.pathname.length - 1) !== "/" && url + "/" == window.location.pathname) {
+            } else if (window.location.pathname.charAt(window.location.pathname.length - 1) !== "/" && url + "/" == window.location.pathname || isLink == false) {
                 var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links");
             } else {
                 var $currentLink = $("<a>").attr("href", url).addClass("link").text(displayName).appendTo("#links");
