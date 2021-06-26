2f4e21e95a28f2f3f5783d4bf0964dc5a31045ee
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 12:39:53 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             $("<audio>").attr("src", "https://25yeht.github.io/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
-        var theInterval = window.setInterval(function() {
+        var dasInterval = window.setInterval(function() {
             $("#cur-aud").remove();
             $("<audio>").attr("src", "https://25yeht.github.io/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
         }, (3000*60) + 50000);
