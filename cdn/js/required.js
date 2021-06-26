c80a98f4a2eb006f0a70e4193df09493415ef82d
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 10:17:37 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -1,6 +1,5 @@
 if(document.getElementsByName("jquery")[0]) {
     console.log("required.js started!");
-    if(window.location.pathname.charAt(window.location.pathname.length - 1) == "/") {
         if(window.location.pathname == "/") {
             console.log("Welcome home!");
             $(document).ready(function() {
@@ -14,21 +13,6 @@ if(document.getElementsByName("jquery")[0]) {
                     }
                 });
             });
-        }
-    } else {
-        console.log("Welcome home!");
-            $(document).ready(function() {
-                $("<button>").text("Play Sky High").attr("id", "pl-sk-hi").appendTo("#ad-js-player-div");
-                var isAppended = false;
-                $("#pl-sk-hi").on("click", function() {
-                    if(isAppended == false) {
-                        $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("controls", true).attr("autoplay", true).appendTo("#ad-js-player-div");
-                    	isAppended = true;
-						$("#pl-sk-hi").remove();
-                    }
-                });
-            });
-        }
 } else {
     console.log("jQuery not found! Maybe try adding the script as the first?");
 }
