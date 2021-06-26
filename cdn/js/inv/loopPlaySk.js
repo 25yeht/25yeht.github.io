2ce17ecf1e8144e64e1376a4f334b2dad6ac5e0b
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-26 12:42:11 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
-            $("<audio>").attr("src", "https://25yeht.github.io/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
+            $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
         var dasInterval = window.setInterval(function() {
             $("#cur-aud").remove();
-            $("<audio>").attr("src", "https://25yeht.github.io/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
+            $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
         }, (3000*60) + 50000);
     });
 });
