9113c4718cc7a3bfa2871410f0e63a2c3b2baebd
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-27 14:34:10 -0500

    Update genNews.js

--- cdn/js/inv/genNews.js
+++ cdn/js/inv/genNews.js
@@ -17,13 +17,7 @@ if(document.getElementsByName("jquery")[0]) {
             $("<h1>").addClass("news").addClass("centered").html(html).appendTo("#news-div");
         }
         //Start Generating News
-        addAnch("Today's News is propaganda: Elderly women scratches her book");
-        addImg("https://www.mozilla.org/media/img/home/2021/gary.f79b62eae19a.png", "Image of the women scratching her book", "30%", null);
-        addP("Granny scratches her book<br><em>Source: <a href='https://www.mozilla.org/media/img/home/2021/gary.f79b62eae19a.png' target='blank'>Mozilla Media</a></em>");
-        $("<br>").appendTo("body");
-        addP("<br><br><em>Ahh, I just love scratching books</em> - Granny");
-        $("<br>").appendTo("body");
-        addP("This woman just <strong>LOVES</strong> scratching her books. She even got fined $1000000.00 at the local library for property damage and valdalism! :D<br>And, instead of paying the fine, the library actully paid her the $1000000.00!!!<br>Thanks for reading!!! <strong>BYE BYE!!! :D</strong>");
+        addAnch("No news today. SOWWY!");
         for(var i = 0; i < 5; i++) {
             $("<br>").appendTo("body");
         }
