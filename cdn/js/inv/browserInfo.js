30c7456a929d9846f4245b9b06e93003061bac2b
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-28 12:20:24 -0500

    Update browserInfo.js

--- cdn/js/inv/browserInfo.js
+++ cdn/js/inv/browserInfo.js
@@ -8,6 +8,7 @@ if(document.getElementsByName("jquery")[0]) {
             /Android/i,
             /webOS/i,
             /iPhone/i,
+			/iPad/i,
             /iPod/i,
             /BlackBerry/i,
             /Windows Phone/i
