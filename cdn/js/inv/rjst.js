8089fc8b8b5e8e7f77462201244068b00eca6f12
25yeht <76498360+25yeht@users.noreply.github.com>
2021-06-29 17:48:34 -0500

    Update rjst.js

--- cdn/js/inv/rjst.js
+++ cdn/js/inv/rjst.js
@@ -5,10 +5,14 @@ if(document.getElementsByName("jquery")[0]) {
         //After the document is fully loaded, you can properly manipulate HTML.
         $("<button>").text("Run (jQuery is included)").attr("id", "rjst-btn").appendTo("#rjst-btn-div");
         $("#rjst-btn").on("click", function() {
-            try {
-                eval($("#notepad").val());
-            } catch(error) {
-                alert("Syntax Error!")
+            if($("#notepad").val()[0].includes("while(true) {") || $("#notepad").val()[0].includes("while(true){") || $("#notepad").val()[0].includes("while(true)|{")) {
+                alert("Woah there buddy, a while true loop will lag this webpage!");
+            } else {
+                try {
+                    eval($("#notepad").val());
+                } catch(error) {
+                    alert("Syntax Error!")
+                }
             }
         });
         $("<div>").attr("id", "dev-console").addClass("dev").html("Here are some links you can copy and paste from:<br><a href='/cdn/js/genLinks.js' target='_blank'>Generate Links</a><br><a href='/cdn/js/inv/genNews.js' target='_blank'>Generate News</a><br><a href='/cdn/js/inv/rjst.js' target='_blank'>The script used to generate this box!</a>").appendTo("#rjst-btn-div");
