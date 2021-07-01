e52b7561053c22e2ae3eb3196d46229b14b350fb
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-01 15:10:06 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -24,4 +24,7 @@ $(document).ready(function() {
             	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
         }, (3000*60) + 50000);
     });
+    window.onbeforeunload = function() {
+        document.title = "25yeht";
+    };
 });
