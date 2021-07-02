8693f45836ae489238685d2e5ef3fae09d99901c
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-02 16:57:55 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
 		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
+		$("<button>").attr("id", "pl-sk-hi").html("Play Energy").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
-        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_Energy_NCS_Release.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
     		$("<button>").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
-			document.title = "Playing: Sky High - 25yeht";
+			document.title = "Playing: Energy - 25yeht";
 			$("#gba").append($("<a>").attr("href", window.location.pathname).text("Get buttons again").addClass("btn-a"));
             $("#pl-sk-hi").remove();
 			$("#lp-sk-hi").remove();
+			$("#gba").on("click", function() {
+				$("#gba").text("Reloading...");
+			});
         });
-    $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+    $("<button>").text("Loop Energy").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
-			document.title = "Playing: Sky High - 25yeht";
-            $("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).appendTo("body");
+			document.title = "Playing: Energy - 25yeht";
+            $("<audio>").attr("src", "/cdn/audio/Elektronomia_Energy_NCS_Release.mp3").attr("autoplay", true).appendTo("body");
             $("<button>").attr("id", "gba").appendTo("#elektronomia-sky-high-js-player-div");
 	    	$("#gba").append($("<a>").attr("href", window.location.pathname).text("Get buttons again").addClass("btn-a"));
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
+			$("#gba").on("click", function() {
+				$("#gba").text("Reloading...");
+			});
         	var dasInterval = window.setInterval(function() {
             	$("#cur-aud").remove();
             	$("<audio>").attr("src", "/cdn/audio/Elektronomia_-_Sky_High_NCS_Release[GetVideo.watch].mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
