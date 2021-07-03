50a99ffe7ba41d9fb96c9901b717b1674ca76a69
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-02 21:04:50 -0500

    Update afvd.js

--- cdn/js/inv/afvd.js
+++ cdn/js/inv/afvd.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/*
     var $video = $("<video>").attr("src", "https://afficient-website.s3-us-west-1.amazonaws.com/remote_student_orientation_quick_deep_review.mp4").text("Sorry, but your browser does not support HTML5 video. :(").attr("controls", true).attr("autoplay", true).width("100%").addClass("centered").appendTo("body");
     //Pretty Printed
     function setRadius(int) {
@@ -13,4 +14,6 @@ $(document).ready(function() {
         setRadius(15);
         $("[type=number]").attr("value", "15");
     });
+	*/
+	$("<h1>").text("Access Denied").appendTo("body");
 });
