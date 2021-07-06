b497a159b6039bd6498c005fa641d27991874749
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-06 11:19:21 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 		//Generate play button and audio
 		$("<button>").attr("id", "pl-sk-hi").html("Play Sunburst").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
-			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
+			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds' target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
         	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
 			document.title = "Playing: Sunburst - 25yeht";
-			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
+			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).appendTo("body");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
