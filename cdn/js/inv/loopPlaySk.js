12d09253411d851e18f7ef07225cadae481b20cc
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-06 10:35:50 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Energy").appendTo("#elektronomia-sky-high-js-player-div");
+		$("<button>").attr("id", "pl-sk-hi").html("Play Sunburst").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
-        	$("<audio>").attr("src", "/cdn/audio/Elektronomia_Energy_NCS_Release.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
+        	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
@@ -12,11 +13,12 @@ $(document).ready(function() {
 				$("#gba").text("Reloading...");
 			});
         });
-    $("<button>").text("Loop Energy").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+    $("<button>").text("Loop Sunburst").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
-			document.title = "Playing: Energy - 25yeht";
-            $("<audio>").attr("src", "/cdn/audio/Elektronomia_Energy_NCS_Release.mp3").attr("autoplay", true).appendTo("body");
+			document.title = "Playing: Sunburst - 25yeht";
+			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
+            $("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).appendTo("body");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
@@ -26,7 +28,7 @@ $(document).ready(function() {
 			});
         	var dasInterval = window.setInterval(function() {
             	$("#cur-aud").remove();
-            	$("<audio>").attr("src", "/cdn/audio/Elektronomia_Energy_NCS_Release.mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
+            	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
         }, (3000*60) + 15000);
     });
     window.onbeforeunload = function() {
