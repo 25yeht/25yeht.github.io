df8d43aa97e53c55cef9ef6799102bb63aa31fa3
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-06 11:34:30 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -4,12 +4,6 @@ $(document).ready(function() {
     	$("#pl-sk-hi").on("click", function() {
 			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds' target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
         	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
-			$("<p>").addClass("container-p").html("Loading...").attr("id", "ldg").appendTo("#elektronomia-sky-high-js-player-div");
-			$("#ldg")[0].addEventListener("loadeddata", function() {
-				if($("#ldg")[0].readyState >= 3) {
-					$("#ldg").remove();
-				}
-			});
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
@@ -25,12 +19,6 @@ $(document).ready(function() {
 			document.title = "Playing: Sunburst - 25yeht";
 			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).appendTo("body");
-			$("<p>").addClass("container-p").html("Loading...").attr("id", "ldg").appendTo("#elektronomia-sky-high-js-player-div");
-			$("#ldg")[0].addEventListener("loadeddata", function() {
-				if($("#ldg")[0].readyState >= 3) {
-					$("#ldg").remove();
-				}
-			});
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
