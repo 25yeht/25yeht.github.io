cb249031ef5c95c32ed867ac761b9a4cf88695a7
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-09 11:52:16 -0500

    Create simple.js

---
+++ cdn/js/tryjs/simple.js
@@ -0,0 +1,6 @@
+alert("Hi! I am an alert. You can call me by using: alert(\"string\");");
+var a = prompt("Hi! I am a prompt. You can call me by using: prompt(\"string\");");
+var b = confirm("You have entered: " + a + `.
+
+Hi! I am a confirmation. You can call me by using: confirm(\"string\");`);
+alert(b);
