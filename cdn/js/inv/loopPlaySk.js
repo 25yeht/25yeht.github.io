e4bf577ff308a4930f1a2976c6f055bbc037385c
25yeht <76498360+25yeht@users.noreply.github.com>
2021-07-14 09:34:40 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Sunburst").appendTo("#elektronomia-sky-high-js-player-div");
+		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
-			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds' target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
-        	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+			console.log("Clicked!");
+			document.title = "Playing: Sky High - 25yeht";
+			$("<p>").addClass("container-p").html("Song: Elektronomia - Sky High [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/skyhigh' target='_blank'>http://ncs.io/skyhigh</a><br>Watch: <a href='http://youtu.be/TW9d8vYrVFQ' target='_blank'>http://youtu.be/TW9d8vYrVFQ</a>").appendTo("#elektronomia-sky-high-js-player-div");
+        	$("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
@@ -13,12 +15,12 @@ $(document).ready(function() {
 				$("#gba").text("Reloading...");
 			});
         });
-    $("<button>").text("Loop Sunburst").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+    $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
-			document.title = "Playing: Sunburst - 25yeht";
-			$("<p>").addClass("container-p").html("Music: Tobu & Itro - Sunburst<br><a href='http://youtube.com/tobuofficial' target='_blank'>http://youtube.com/tobuofficial</a><br><a href='https://www.youtube.com/user/officialitro' target='_blank'>https://www.youtube.com/user/officialitro</a><br>Released by NCS <a href='https://www.youtube.com/NoCopyrightSounds'target='_blank'>https://www.youtube.com/NoCopyrightSounds</a>").appendTo("#elektronomia-sky-high-js-player-div");
-            $("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).appendTo("body");
+			document.title = "Playing: Sky High - 25yeht";
+			$("<p>").addClass("container-p").html("Song: Elektronomia - Sky High [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/skyhigh' target='_blank'>http://ncs.io/skyhigh</a><br>Watch: <a href='http://youtu.be/TW9d8vYrVFQ' target='_blank'>http://youtu.be/TW9d8vYrVFQ</a>").appendTo("#elektronomia-sky-high-js-player-div");
+            $("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).appendTo("body");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
@@ -28,7 +30,7 @@ $(document).ready(function() {
 			});
         	var dasInterval = window.setInterval(function() {
             	$("#cur-aud").remove();
-            	$("<audio>").attr("src", "/cdn/audio/Tobu & Itro - Sunburst.mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
+            	$("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
         }, (3000*60) + 5500);
     });
     window.onbeforeunload = function() {
