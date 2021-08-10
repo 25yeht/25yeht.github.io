32ce7782c42c29df5447495998572d92c5f90688
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 15:22:12 -0500

    Update loopSong.js

--- cdn/js/inv/loopSong.js
+++ cdn/js/inv/loopSong.js
@@ -4,9 +4,18 @@ if(document.getElementsByName("jquery")[0]) {
     $(document).ready(() => {
         //After the document is fully loaded, you can properly manipulate HTML.
 		var l = "";
+		var urls = {
+			everything: "/cdn/audio/music/everything.mp3",
+			mp3: "/cdn/audio/music/mp3.mp3",
+			skyline: "/cdn/audio/music/skyline.mp3"
+		};
         $("#play").click(() => {
 			if($("#url").val()) {
-				$("#aud").attr("src", $("#url").val())[0].play();
+				if(urls[$("#url").val().toLowerCase()]) {
+					$("#aud").attr("src", urls[$("#url").val().toLowerCase()])[0].play();
+				} else {
+					$("#aud").attr("src", $("#url").val())[0].play();
+				}
 				$("#url").attr("placeholder", $("#url").val());
 				l = $("#url").val();
 			} else {
@@ -17,8 +26,11 @@ if(document.getElementsByName("jquery")[0]) {
 			if(e.keyCode == 13) {
 				alert(2);
 				if($("#url").val()) {
-					$("#aud").attr("src", $("#url").val())[0].play();
-					$("#url").attr("placeholder", $("#url").val());
+					if(urls[$("#url").val().toLowerCase()]) {
+						$("#aud").attr("src", urls[$("#url").val().toLowerCase()])[0].play();
+					} else {
+						$("#aud").attr("src", $("#url").val())[0].play();
+					}
 					l = $("#url").val();
 				} else {
 					$("#aud").attr("src", l)[0].play();
