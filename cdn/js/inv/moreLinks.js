161d6bfdbba0b328645da2ca6ed034de9e88583f
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 12:06:51 -0500

    Update moreLinks.js

--- cdn/js/inv/moreLinks.js
+++ cdn/js/inv/moreLinks.js
@@ -19,6 +19,7 @@ if(document.getElementsByName("jquery")[0]) {
 		addLink("Run JS from a link", "/fun/run-js-from-link/");
 		addLink("25Browser", "/fun/25browser/");
 		addLink("Display an Image", "/fun/display-img/");
+		addLink("Create a playlist", "/fun/make-playlist/");
         });
 } else {
     console.log("jQuery not found!")
