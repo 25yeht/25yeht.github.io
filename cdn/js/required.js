f7e3c60a6d98f56cf796daf6b46d69597b741b03
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 12:02:58 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -1,9 +1,10 @@
 if(document.getElementsByName("jquery")[0]) {
     console.log("required.js started!");
     console.log("Welcome home!");
-    
-    $(document).ready(function() {
-		
+    $(document).ready(() => {
+		if(window.location.href.toLowerCase().includes("index.html")) {
+			window.location.href = window.location.href.replace("index.html", "");
+		}
     });
 } else {
     console.log("jQuery not found! Maybe try adding the script as the first?");
