7ef4cc315da43d4c3d077868435d229b18390f05
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 13:50:49 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(() => {
 		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Sky High").appendTo("#elektronomia-sky-high-js-player-div");
+		$("<button>").attr("id", "pl-sk-hi").html("Play Everything").appendTo("#elektronomia-sky-high-js-player-div");
     	$("#pl-sk-hi").on("click", function() {
 			console.log("Clicked!");
 			document.title = "Playing: Sky High - 25yeht";
-			$("<p>").addClass("container-p").html("Song: Elektronomia - Sky High [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/skyhigh' target='_blank'>http://ncs.io/skyhigh</a><br>Watch: <a href='http://youtu.be/TW9d8vYrVFQ' target='_blank'>http://youtu.be/TW9d8vYrVFQ</a>").appendTo("#elektronomia-sky-high-js-player-div");
-        	$("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+			$("<p>").addClass("container-p").html("Song: Diamond Eyes - Everything [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/Everything' target='_blank'>http://ncs.io/Everything</a><br>Watch: <a href='http://youtu.be/_XspQUK22-U' target='_blank'>http://youtu.be/_XspQUK22-U</a>").appendTo("#elektronomia-sky-high-js-player-div");
+        	$("<audio>").attr("src", "/cdn/audio/everything.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
@@ -15,12 +15,12 @@ $(document).ready(function() {
 				$("#gba").text("Reloading...");
 			});
         });
-    $("<button>").text("Loop Sky High").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+    $("<button>").text("Loop Everything").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
             console.log("Clicked!");
 			document.title = "Playing: Sky High - 25yeht";
-			$("<p>").addClass("container-p").html("Song: Elektronomia - Sky High [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/skyhigh' target='_blank'>http://ncs.io/skyhigh</a><br>Watch: <a href='http://youtu.be/TW9d8vYrVFQ' target='_blank'>http://youtu.be/TW9d8vYrVFQ</a>").appendTo("#elektronomia-sky-high-js-player-div");
-            $("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).appendTo("body");
+			$("<p>").addClass("container-p").html("Song: Diamond Eyes - Everything [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/Everything' target='_blank'>http://ncs.io/Everything</a><br>Watch: <a href='http://youtu.be/_XspQUK22-U' target='_blank'>http://youtu.be/_XspQUK22-U</a>").appendTo("#elektronomia-sky-high-js-player-div");
+            $("<audio>").attr("src", "/cdn/audio/everything.mp3").attr("autoplay", true).attr("loop", true).appendTo("body");
             $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
             $("#pl-sk-hi").remove();
             $("#lp-sk-hi").remove();
@@ -28,12 +28,8 @@ $(document).ready(function() {
 				window.location.pathname = window.location.pathname;
 				$("#gba").text("Reloading...");
 			});
-        	var dasInterval = window.setInterval(function() {
-            	$("#cur-aud").remove();
-            	$("<audio>").attr("src", "/cdn/audio/elekskhi.mp3").attr("autoplay", true).attr("id", "cur-aud").appendTo("body");
-        }, (3000*60) + 56000);
     });
-    window.onbeforeunload = function() {
+    window.onbeforeunload = () => {
         document.title = "25yeht";
     };
 });
