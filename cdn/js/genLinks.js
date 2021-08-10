4681a6ed9e0b49da8eee9f11a0524d43319a49f2
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 14:23:08 -0500

    Update genLinks.js

--- cdn/js/genLinks.js
+++ cdn/js/genLinks.js
@@ -24,7 +24,7 @@ if(document.getElementsByName("jquery")[0]) {
         addLink("Run JS on the web", "/fun/run-js-on-web");
         addLink("Info", "/info/");
 		addLink("More Links", "/links/");
-        addExtLink("25yeht Beta", "https://25yehtgithubio.a25yeht.repl.co/");
+        addLink("25yeht Beta", "https://25yehtgithubio.a25yeht.repl.co/");
         addLink("This site is under development.", null, false);
         });
 } else {
