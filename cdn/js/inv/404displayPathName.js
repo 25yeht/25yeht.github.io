a50226db0b61ba61d53149c747bdd435963696e1
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 14:49:03 -0500

    Update 404displayPathName.js

--- cdn/js/inv/404displayPathName.js
+++ cdn/js/inv/404displayPathName.js
@@ -3,7 +3,14 @@ if(document.getElementsByName("jquery")[0]) {
     //When the document is loaded, do code inside of function in parameter.
     $(document).ready(function() {
         //After the document is fully loaded, you can properly manipulate HTML.
-        $("#pathname").text("Got a 404 response error when trying to process pathname '" + window.location.pathname + "'. Sorry! :(").css("font-family", "monospace").addClass("centered").css("text-align", "center");
+		if(!window.location.search.toLowerCase().includes("switch=true")) {
+        	$("#pathname").text("Got a 404 response error when trying to process pathname '" + window.location.pathname + "'. Sorry! :(").css("font-family", "monospace").addClass("centered").css("text-align", "center");
+		} else {
+			$("#pathname").text("Got a 404 response error when trying to process pathname '" + window.location.pathname + "'. Note: Some beta/stable features may not be available on both sites.").css("font-family", "monospace").addClass("centered").css("text-align", "center");
+			$("<button>").text("Go back").addClass("centered").click(( => {
+				history.back();
+			}));
+		}
     });
 } else {
     console.log("jQuery not found. Maybe put [name=template.js] in front of it?")
