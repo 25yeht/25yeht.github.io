36dd1ed39475453d1f9c7dbf437f075db6038e02
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-11 17:18:12 -0500

    Update rjst.js

--- cdn/js/inv/rjst.js
+++ cdn/js/inv/rjst.js
@@ -3,9 +3,32 @@ if(document.getElementsByName("jquery")[0]) {
     //When the document is loaded, do code inside of function in parameter.
     $(document).ready(function() {
         //After the document is fully loaded, you can properly manipulate HTML.
+		function enableTab(id) {
+			var el = document.getElementById(id);
+			el.onkeydown = function(e) {
+				if (e.keyCode === 9) { // tab was pressed
+
+					// get caret position/selection
+					var val = this.value,
+						start = this.selectionStart,
+						end = this.selectionEnd;
+
+					// set textarea value to: text before caret + tab + text after caret
+					this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+					// put caret at right position again
+					this.selectionStart = this.selectionEnd = start + 1;
+
+					// prevent the focus lose
+					return false;
+
+				}
+			};
+		}
+		enableTab("notepad");
         $("<button>").text("Run (jQuery is included)").attr("id", "rjst-btn").appendTo("#rjst-btn-div");
         $("#rjst-btn").on("click", function() {
-            if($("#notepad").val()[0].includes("while(true) {") || $("#notepad").val()[0].includes("while(true){") || $("#notepad").val()[0].includes("while(true)|{")) {
+            if($("#notepad").val()[0].includes("while(true) {") || $("#notepad").val().includes("while(true){") || $("#notepad").val().includes("while(true)|{")) {
                 alert("Woah there buddy, a while true loop will lag this webpage!");
             } else {
                 try {
