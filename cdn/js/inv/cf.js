b8c4b00a8304b72270ee61b68ee2348470583040
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 19:03:34 -0500

    Update cf.js

--- cdn/js/inv/cf.js
+++ cdn/js/inv/cf.js
@@ -1,7 +1,7 @@
 if(document.getElementsByName("jquery")[0]) {
     console.log("cf.js started!");
     //When the document is loaded, do code inside of function in parameter.
-    $(document).ready(function() {
+    $(document).ready(() => {
         //After the document is fully loaded, you can properly manipulate HTML.
         function previewFile() {
 			var preview = document.querySelector('img');
@@ -11,13 +11,14 @@ if(document.getElementsByName("jquery")[0]) {
 			reader.addEventListener("load", function () {
 				// convert image file to base64 string
 				preview.src = reader.result;
+				preview.style.borderRadius = "12px";
 			}, false);
 
 			if (file) {
 				reader.readAsDataURL(file);
 			}
 		}
-		$("#prev").click(function() {
+		$("#prev").click(() => {
 			previewFile();
 		});
     });
