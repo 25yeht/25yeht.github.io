e01f7669cfa452055556affcf1f51d70813a65f7
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-11 10:16:01 -0500

    Update genNews.js

--- cdn/js/inv/genNews.js
+++ cdn/js/inv/genNews.js
@@ -17,7 +17,7 @@ if(document.getElementsByName("jquery")[0]) {
             $("<h1>").addClass("news").addClass("centered").html(html).appendTo("#news-div");
         }
         //Start Generating News
-        /*addAnch("Western media lies about china");
+        addAnch("Western media lies about china");
 		addH("Here is a fake 哔哔C story about china:");
 		addP("<a style='color: red;' id='aa' href='https://www.bbc.co.uk/news/business-57419071'>Government ‘dragging feet’ on China forced labo<b><i style='color: black; font-size: 20px;'>u</i></b>r</a>");
 		$("#aa").click(e => {
@@ -27,10 +27,7 @@ if(document.getElementsByName("jquery")[0]) {
 				a.document.body.innerHTML = a.document.body.innerHTML.replaceAll("'", "");
 				clearTimeout(ab);
 			}, 200)
-		});*/
-		$("#news-div").addClass("emergency-alert");
-		addAnch("<span style='color: red; font-weight: 35px; font-family: arial;'>SEVERE THUNDERSTORM REPORTED IN NOTHERN ILLINOIS</span><br><br>");
-		addP("<span style='color: red; font-weight: 35px; font-family: arial;'>SEVERE THUNDERSTORM WATCH 420 IS IN EFFECT UNTIL 1000 PM CDT<br>FOR THE FOLLOWING LOCATIONS<br>IL<br><br>.    ILLINOIS COUNTIES INCLUDED ARE<br>BOONE                BUREAU              CARROLL<br>COOK                 DE KALB             DUPAGE<br>GRUNDY               HENRY               JO DAVIESS<br>KANE                 KENDALL             LAKE<br>LA SALLE             LEE                 MCHENRY<br>OGLE                 PUTNAM              STEPHENSON<br>WHITESIDE            WILL                WINNEBAGO<br><br>SEVERE THUNDERSTORM WATCH 420 REMAINS VALID UNTIL 10 PM CDT THIS<br>EVENING FOR THE FOLLOWING AREAS<br>IN ILLINOIS THIS WATCH INCLUDES 6 COUNTIES<br>IN NORTHEAST ILLINOIS<br>COOK                  DUPAGE                GRUNDY<br>KENDALL               LAKE IL               WILL<br>THIS INCLUDES THE CITIES OF BOLINGBROOK, BUFFALO GROVE,<br>CAROL STREAM, CHICAGO, COAL CITY, DOWNERS GROVE, EVANSTON,<br>GURNEE, JOLIET, LEMONT, LOMBARD, MINOOKA, MORRIS, MUNDELEIN,<br>NAPERVILLE, OAK LAWN, ORLAND PARK, OSWEGO, PARK FOREST, PLANO,<br>SCHAUMBURG, WAUKEGAN, WHEATON, WILMINGTON, AND YORKVILLE.<br><br>From the <a href='https://weather.gov' target='_blank' style='color: purple; text-decoration: underline;'>National Weather Service</a> at 4:00 PM 8/10/2021 US format and US date and time centeral time.</span>");
+		});
         for(var i = 0; i < 5; i++) {
             $("<br>").appendTo("body");
         }
