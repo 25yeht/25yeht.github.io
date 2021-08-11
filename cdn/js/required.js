e7559394e036d154044539eacbf410c9296f7b9a
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-10 20:26:21 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -3,8 +3,9 @@ if(document.getElementsByName("jquery")[0]) {
     console.log("Welcome home!");
     $(document).ready(() => {
 		if(window.location.href.toLowerCase().includes("index.html")) {
-			window.location.href = window.location.href.replace("index.html", "");
+			//window.location.href = window.location.href.replace("index.html", "");
 		}
+		$("<h2>").text("SEVERE THUNDERSTORM REPORTED IN NOTHERN ILLINOIS AT 4:00 PM CENTERAL TIME 8/10/2021 US DATE FORMAT AND US DATE AND TIME").addClass("err-msg").appendTo("body").css("font-family", "arial, helvetica, sans-serif");
     });
 } else {
     console.log("jQuery not found! Maybe try adding the script as the first?");
