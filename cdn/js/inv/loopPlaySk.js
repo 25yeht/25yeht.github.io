81aca664d7435dce83dfaea8f5f8318474f6b582
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-21 13:33:32 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -10,7 +10,7 @@ $(document).ready(() => {
 		console.log(logText);
 		document.title = "Playing: " + name +" - 25yeht";
 		$("<p>").addClass("container-p").html(attrText).appendTo("#elektronomia-sky-high-js-player-div");
-		$("<audio>").attr("src", url).attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+		var audio = $("<audio>").attr("src", url).attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
 		$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
 		$("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
 		$("#pl-sk-hi").remove();
@@ -25,7 +25,7 @@ $(document).ready(() => {
 		console.log(logText);
 		document.title = "Playing: " + name + " - 25yeht";
 		$("<p>").addClass("container-p").html(attrText).appendTo("#elektronomia-sky-high-js-player-div");
-		$("<audio>").attr("src", url).attr("autoplay", true).attr("loop", true).appendTo("body");
+		var audio = $("<audio>").attr("src", url).attr("autoplay", true).attr("loop", true).appendTo("body");
 		$("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
 		$("#pl-sk-hi").remove();
 		$("#lp-sk-hi").remove();
@@ -34,6 +34,20 @@ $(document).ready(() => {
 			$("#gba").text("Reloading...");
 		});
     });
+	//Detect 25yeht PPLS plugin
+	var t25yehtPluginDetectorFaded = false;
+	var t25yeht_PPLSPluginDetectionInterval = setInterval(_ => {
+		if(this) {
+			if(this["t25yeht_PPLS:RUNNING"] && !t25yehtPluginDetectorFaded) {
+				audio.remove();
+				$("#elektronomia-sky-high-js-player-div").hide().html("<h1>Plugin detected!</h1><br>").fadeIn();
+				t25yehtPluginDetectorFaded = true;
+			}
+			if(t25yehtPluginDetectorFaded) {
+				$("#elektronomia-sky-high-js-player-div").html("<h1>Plugin detected! Now playing: " + this["t25yeht_PPLS:SONGNAMES"][urlIndex] + "</h1><br>");
+			}
+		}
+	}, 100);
     window.onbeforeunload = () => {
         document.title = "25yeht";
     };
