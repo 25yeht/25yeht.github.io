680c8c3869be5d8795611cb79b65f44066f3c9f2
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-21 10:20:30 -0500

    Update loopPlaySk.js

--- cdn/js/inv/loopPlaySk.js
+++ cdn/js/inv/loopPlaySk.js
@@ -1,33 +1,38 @@
 $(document).ready(() => {
-		//Generate play button and audio
-		$("<button>").attr("id", "pl-sk-hi").html("Play Everything").appendTo("#elektronomia-sky-high-js-player-div");
-    	$("#pl-sk-hi").on("click", function() {
-			console.log("Clicked!");
-			document.title = "Playing: Everything - 25yeht";
-			$("<p>").addClass("container-p").html("Song: Diamond Eyes - Everything [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/Everything' target='_blank'>http://ncs.io/Everything</a><br>Watch: <a href='http://youtu.be/_XspQUK22-U' target='_blank'>http://youtu.be/_XspQUK22-U</a>").appendTo("#elektronomia-sky-high-js-player-div");
-        	$("<audio>").attr("src", "/cdn/audio/music/everything.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
-			$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
-            $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
-            $("#pl-sk-hi").remove();
-            $("#lp-sk-hi").remove();
-			$("#gba").on("click", function() {
-				location.reload();
-				$("#gba").text("Reloading...");
-			});
-        });
-    $("<button>").text("Loop Everything").attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
+	//Define song variables
+	var name = "Sunburst";
+	var url = "/cdn/audio/Tobu & Itro - Sunburst.mp3";
+	var attrText = "Song: Tobu & Itro - Sunburst [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: N/A<br>Watch: <a href=\"javascript:open(&quot;http://youtu.be/4lXBHD5C8do&quot;, &quot;_blank&quot;, &quot;width=450, height=650&quot)\">http://youtu.be/4lXBHD5C8do</a>";
+	var logText = "Play/loop button clicked!";
+	//Generate play button and audio
+	$("<button>").attr("id", "pl-sk-hi").html("Play " + name).appendTo("#elektronomia-sky-high-js-player-div");
+	$("#pl-sk-hi").on("click", function() {
+		console.log(logText);
+		document.title = "Playing: " + name +" - 25yeht";
+		$("<p>").addClass("container-p").html(attrText).appendTo("#elektronomia-sky-high-js-player-div");
+		$("<audio>").attr("src", "/cdn/audio/music/everything.mp3").attr("autoplay", true).attr("controls", true).appendTo("#elektronomia-sky-high-js-player-div");
+		$("<br>").appendTo("#elektronomia-sky-high-js-player-div");
+		$("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
+		$("#pl-sk-hi").remove();
+		$("#lp-sk-hi").remove();
+		$("#gba").on("click", function() {
+			location.reload();
+			$("#gba").text("Reloading...");
+		});
+	});
+    $("<button>").text("Loop " + name).attr("id", "lp-sk-hi").appendTo("#elektronomia-sky-high-js-player-div");
     $("#lp-sk-hi").on("click", function() {
-            console.log("Clicked!");
-			document.title = "Playing: Everything - 25yeht";
-			$("<p>").addClass("container-p").html("Song: Diamond Eyes - Everything [NCS Release]<br>Music provided by NoCopyrightSounds<br>Free Download/Stream: <a href='http://ncs.io/Everything' target='_blank'>http://ncs.io/Everything</a><br>Watch: <a href='http://youtu.be/_XspQUK22-U' target='_blank'>http://youtu.be/_XspQUK22-U</a>").appendTo("#elektronomia-sky-high-js-player-div");
-            $("<audio>").attr("src", "/cdn/audio/music/everything.mp3").attr("autoplay", true).attr("loop", true).appendTo("body");
-            $("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
-            $("#pl-sk-hi").remove();
-            $("#lp-sk-hi").remove();
-			$("#gba").on("click", function() {
-				location.reload();
-				$("#gba").text("Reloading...");
-			});
+		console.log(logText);
+		document.title = "Playing: " + name + " - 25yeht";
+		$("<p>").addClass("container-p").html(attrText).appendTo("#elektronomia-sky-high-js-player-div");
+		$("<audio>").attr("src", "/cdn/audio/music/everything.mp3").attr("autoplay", true).attr("loop", true).appendTo("body");
+		$("<button>").attr("id", "gba").text("Get Buttons Again").appendTo("#elektronomia-sky-high-js-player-div");
+		$("#pl-sk-hi").remove();
+		$("#lp-sk-hi").remove();
+		$("#gba").on("click", function() {
+			location.reload();
+			$("#gba").text("Reloading...");
+		});
     });
     window.onbeforeunload = () => {
         document.title = "25yeht";
