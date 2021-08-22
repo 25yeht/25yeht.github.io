5683bf9551a4f642ad78ed0427abcbff4102c0ab
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-22 12:08:26 -0500

    Update genLinks.js

--- cdn/js/genLinks.js
+++ cdn/js/genLinks.js
@@ -4,18 +4,62 @@ if(document.getElementsByName("jquery")[0]) {
         console.log("Document is ready!");
         function addLink(displayName, url, isLink) {
             if(window.location.pathname.charAt(window.location.pathname.length - 1) == "/" && url == window.location.pathname) {
-                var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links");
+                var $currentLink = $("<span>").addClass("link").text(displayName).css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none"
+				}).appendTo("#links");
             } else if (window.location.pathname.charAt(window.location.pathname.length - 1) !== "/" && url + "/" == window.location.pathname || isLink == false) {
-                var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links");
+                var $currentLink = $("<span>").addClass("link").text(displayName).css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none"
+				}).appendTo("#links");
             } else {
-                var $currentLink = $("<a>").attr("href", url).addClass("link").text(displayName).appendTo("#links");
+                var $currentLink = $("<span>").attr("href", url).addClass("link").text(displayName).appendTo("#links").css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none",
+					"cursor": "pointer"
+				}).click(_ => {
+					location.href = url;
+				}).appendTo("#links");
             }
         }
         function addExtLink(displayName, url) {
-            var $currentLink = $("<a>").attr("href", url).attr("target", "_blank").addClass("link").text(displayName).appendTo("#links");
+            var $currentLink = $("<span>").attr("href", url).attr("target", "_blank").addClass("link").text(displayName).css({
+				"-webkit-user-drag": "none",
+				"-moz-user-drag": "none",
+				"-ms-user-drag": "none",
+				"-o-user-drag": "none",
+				"user-drag": "none",
+				"user-select": "none",
+				"cursor": "pointer"
+			}).click(_ => {
+				location.href = url;
+			}).appendTo("#links");
 		}
 		function alsp(displayName, hostname) {
-			var $currentLink = $("<a>").attr("href", "https://" + hostname + window.location.pathname + "?switch=true").addClass("link").text(displayName).appendTo("#links");
+			var $currentLink = $("<span>").attr("href", "https://" + hostname + window.location.pathname + "?switch=true").addClass("link").text(displayName).css({
+				"-webkit-user-drag": "none",
+				"-moz-user-drag": "none",
+				"-ms-user-drag": "none",
+				"-o-user-drag": "none",
+				"user-drag": "none",
+				"user-select": "none",
+				"cursor": "pointer"
+			}).click(_ => {
+				location.href = "https://" + hostname + location.pathname + "?switch=true";
+			}).appendTo("#links");
 		}
         //Add the links here addLink(Text Displayed, Real URL);
         addLink("Home", "/");
@@ -36,9 +80,9 @@ if(document.getElementsByName("jquery")[0]) {
 			userDrag: "none"
 		});
 		if(!document.title.toLowerCase().includes("404 not found")) {
-			alsp("25yeht Beta", "25yehtgithubio.a25yeht.repl.co");
+			alsp("25yeht Stable", "25yeht.github.io");
 		} else {
-			addLink("25yeht Beta", "https://25yehtgithubio.a25yeht.repl.co/");
+			addLink("25yeht Stable", "https://25yeht.github.io");
 		}
         addLink("This site is under development.", null, false);
         });
