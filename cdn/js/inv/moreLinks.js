5f5ac6effac1b7c377a9a4c69b59c04c5f2b8010
25yeht <76498360+25yeht@users.noreply.github.com>
2021-08-22 12:07:58 -0500

    Update moreLinks.js

--- cdn/js/inv/moreLinks.js
+++ cdn/js/inv/moreLinks.js
@@ -4,15 +4,49 @@ if(document.getElementsByName("jquery")[0]) {
         console.log("Document is ready!");
         function addLink(displayName, url, isLink) {
             if(window.location.pathname.charAt(window.location.pathname.length - 1) == "/" && url == window.location.pathname) {
-                var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links2");
-            } else if (window.location.pathname[window.location.pathname.length - 1] !== "/" && url + "/" == window.location.pathname || isLink == false) {
-                var $currentLink = $("<span>").addClass("link").text(displayName).appendTo("#links2");
+                var $currentLink = $("<span>").addClass("link").text(displayName).css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none"
+				}).appendTo("#links2");
+            } else if (window.location.pathname.charAt(window.location.pathname.length - 1) !== "/" && url + "/" == window.location.pathname || isLink == false) {
+                var $currentLink = $("<span>").addClass("link").text(displayName).css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none"
+				}).appendTo("#links2");
             } else {
-                var $currentLink = $("<a>").attr("href", url).addClass("link").text(displayName).appendTo("#links2");
+                var $currentLink = $("<span>").attr("href", url).addClass("link").text(displayName).appendTo("#links").css({
+					"-webkit-user-drag": "none",
+					"-moz-user-drag": "none",
+					"-ms-user-drag": "none",
+					"-o-user-drag": "none",
+					"user-drag": "none",
+					"user-select": "none",
+					"cursor": "pointer"
+				}).click(_ => {
+					location.href = url;
+				}).appendTo("#links2");
             }
         }
         function addExtLink(displayName, url) {
-            var $currentLink = $("<a>").attr("href", url).attr("target", "_blank").addClass("link").text(displayName).appendTo("#links2");
+            var $currentLink = $("<span>").attr("href", url).attr("target", "_blank").addClass("link").text(displayName).css({
+				"-webkit-user-drag": "none",
+				"-moz-user-drag": "none",
+				"-ms-user-drag": "none",
+				"-o-user-drag": "none",
+				"user-drag": "none",
+				"user-select": "none",
+				"cursor": "pointer"
+			}).click(_ => {
+				location.href = url;
+			}).appendTo("#links-2");
 		}
         //Add the links here addLink(Text Displayed, Real URL);
         addExtLink("README", "/README.md");
@@ -22,6 +56,7 @@ if(document.getElementsByName("jquery")[0]) {
 		addLink("Loop a song!", "/fun/loop-song/");
 		addLink("Easy Notepad", "/fun/easy-notepad");
 		addLink("Interactive 7-Minute Workout", "/fun/workout-activity/");
+		addLink("Search", "/search/");
 
 		$("a").css({
 			userSelect: "none",
@@ -32,5 +67,5 @@ if(document.getElementsByName("jquery")[0]) {
 		});
         });
 } else {
-    console.log("jQuery not found!")
+    console.log("jQuery not found!");
 }
