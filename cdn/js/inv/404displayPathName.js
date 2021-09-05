31fd2ae5ce375b0460f0bf23937ecd78fa73f8b1
25yeht <76498360+25yeht@users.noreply.github.com>
2021-09-05 10:34:08 -0500

    Update 404displayPathName.js

--- cdn/js/inv/404displayPathName.js
+++ cdn/js/inv/404displayPathName.js
@@ -3,13 +3,13 @@ if(document.getElementsByName("jquery")[0]) {
     //When the document is loaded, do code inside of function in parameter.
     $(document).ready(() => {
         //After the document is fully loaded, you can properly manipulate HTML.
-		if(!window.location.search.toLowerCase().includes("switch=true")) {
+		if(new URL(location.href).searchParams.get("switch") != true) {
         	$("#pathname").text("Got a 404 response error when trying to process pathname '" + window.location.pathname + "'. Sorry! :(").css("font-family", "monospace").addClass("centered").css("text-align", "center");
 		} else {
 			$("#pathname").text("Got a 404 response error when trying to process pathname '" + window.location.pathname + "'. Note: Some beta/stable features may not be available on both sites.").css("font-family", "monospace").addClass("centered").css("text-align", "center");
 			$("<button>").text("Go back").addClass("centered").click(() => {
 				history.back();
-			});
+			}).appendTo("#pathname");
 		}
     });
 } else {
