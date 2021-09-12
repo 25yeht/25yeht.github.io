c17199e97de0d8acf3f3349f8b3da6a62189be81
25yeht <76498360+25yeht@users.noreply.github.com>
2021-09-12 12:09:52 -0500

    Update required.js

--- cdn/js/required.js
+++ cdn/js/required.js
@@ -12,6 +12,14 @@ if(document.getElementsByName("jquery")[0]) {
 				$("input").blur();
 			}
 		});
+		//Detect slow connection
+		var slowLoad = setTimeout(function() {
+			alert("Hmm... Your connection to the server is slow so some of the fonts/scripts may not load.");
+		}, 250);
+
+		addEventListener('load', function() {
+			clearTimeout(slowLoad);
+		}, false);
     });
 } else {
     console.log("jQuery not found! Maybe try adding the script as the first?");
